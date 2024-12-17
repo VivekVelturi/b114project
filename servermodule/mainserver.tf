525297e68f374bbee51c6824e438c0b11bc82203
VivekVelturi <vivek.velturi@gmail.com>
2024-12-17 10:20:47 +0530

    updated servercode

--- servermodule/mainserver.tf
+++ servermodule/mainserver.tf
@@ -62,7 +62,7 @@ resource "aws_instance" "terraformproject" {
   root_block_device {
     
     volume_type = "gp3"
-    volume_size = 8
+    volume_size = 10
     delete_on_termination = true
   }
   tags = {
